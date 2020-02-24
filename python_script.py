f7f1269d22522a69290e3f071cbc386cb1abbfa6
Kamal Bhagat <kjb24@njit.edu>
2020-02-24 17:57:56 -0500

    'Feature-added-python-script'

---
+++ python_script.py
@@ -0,0 +1,8 @@
+# Converting temperature to fahrenheit from celsius values
+
+# Temperature value in Celsius
+Celsius = 20.0
+
+# Convert Temperature value to Fahrenheit
+Fahrenheit = (Celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(Celsius,Fahrenheit))
